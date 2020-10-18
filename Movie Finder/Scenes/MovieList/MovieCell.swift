882988996791a22486ac099674e405ef006ad279
Esraa Gamal <esraa.gamal98@gmail.com>
2020-10-18 14:26:43 +0200

    Created basic UI

---
+++ Movie Finder/Scenes/MovieList/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Movie Finder
+//
+//  Created by Esraa Gamal on 10/18/20.
+//  Copyright © 2020 Esraa Gamal. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
